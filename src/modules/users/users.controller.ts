c75d2ea917c30be2ebb14b8c6accddc8ab776530
eliasgermoliato <eliasgermoliato@hotmail.com>
2023-07-30 12:09:59 -0300

    feat: criando recurso para buscar informações do usuário atual

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/currentUser')
+  currentUser(@Req() request: any) {
+    const userId = request.userId;
+
+    return this.usersService.getUserById(userId);
+  }
 }
